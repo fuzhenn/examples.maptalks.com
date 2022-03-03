710abbc475a0f95708a9e5daef74e5469b87b95d
fuzhenn <fuzhen@maptalks.org>
2022-03-03 22:40:25 +0800

    缩短sceneConfig写法

--- src/config/codes/gltflayer/gltf-util.ts
+++ src/config/codes/gltflayer/gltf-util.ts
@@ -25,50 +25,30 @@ const map = `const map = new maptalks.Map('map', {
 });
 `;
 
-const sceneConfig = `const sceneConfig = {
-    "environment": {
-        "enable": true,
-        "mode": 1,
-        "level": 1,
-        "brightness": 1
+const sceneConfig = `
+const sceneConfig = {
+    environment: { enable: true, mode: 1, level: 1, brightness: 1 },
+    shadow: { enable: true, opacity: 0.5, color: [0, 0, 0] },
+    postProcess: {
+        enable: true,
+        antialias: { enable: true },
+        ssr: { enable: true },
+        bloom: { enable: true },
+        outline: { enable: true }
     },
-    "shadow": {
-        "enable": true,
-        "opacity": 0.5,
-        "color": [0, 0, 0]
-    },
-    "postProcess": {
-            "enable": true,
-            "antialias": {
-              "enable": true,
-              "taa": true,
-              "jitterRatio": 0.25
-            },
-            "ssr": {
-              "enable": true
-            },
-            "bloom": {
-                "enable": true
-            },
-            "outline": {
-                "enable": true
-            }
-        },
-        "ground": {
-            "enable": true,
-            "renderPlugin": {
-              "type": "lit"
-            },
-            "symbol": {
-              "polygonOpacity": 1,
-              "material": {
-                "baseColorFactor": [0.4823529411764706, 0.4823529411764706, 0.4823529411764706, 1],
-                "hsv": [0, 0, -0.532],
-                "roughnessFactor": 0.22,
-                "metallicFactor": 0.58
-              }
-            }
+    ground: {
+        enable: true,
+        renderPlugin: { type: 'lit' },
+        symbol: {
+          polygonOpacity: 1,
+          material: {
+            baseColorFactor: [0.4823529411764706, 0.4823529411764706, 0.4823529411764706, 1],
+            hsv: [0, 0, -0.532],
+            roughnessFactor: 0.22,
+            metallicFactor: 0.58
+          }
         }
+    }
 };`;
 
 export { map, sceneConfig };
