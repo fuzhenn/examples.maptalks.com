de5eb4e3699c46eaf7abd52614a205c8b2292943
yy923683900 <923683900@qq.com>
2022-03-03 10:13:11 +0800

    :lipstick:优化左侧列表样式

--- src/components/SideBar/style.ts
+++ src/components/SideBar/style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   width: 260px;
-  padding: 10px;
+  padding: 10px 10px 80px 10px;
   background: #3e3e3e;
   overflow-y: scroll;
 `;
