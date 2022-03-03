8a92d9bbf40d76c331fe3b2e22fe787b42697271
yy923683900 <923683900@qq.com>
2022-03-03 15:57:54 +0800

    :bug:优化载入geojson后的数据样式

--- src/config/codes/vtlayer/layer-operation/convert-geo.ts
+++ src/config/codes/vtlayer/layer-operation/convert-geo.ts
@@ -12,54 +12,87 @@ body {
   height: 100%;
 }`;
 
-`function convert(data) {
+const jsCode = `const map = new maptalks.Map("map", {
+  center: [-74.00912099912109, 40.71107610933129],
+  zoom: 16,
+});
+  
+const geo = new maptalks.GeoJSONVectorTileLayer("geo", {
+  data: {
+    properties: {
+      adcode: "110000",
+      name: "北京市",
+      center: [116.405285, 39.904989],
+      centroid: [116.41995, 40.18994],
+      childrenNum: 16,
+      level: "province",
+      parent: { adcode: 100000 },
+      subFeatureIndex: 0,
+      acroutes: [100000]
+    },
+    geometry: {
+      type: "Point",
+      coordinates: [116.405285, 39.904989]
+    }
+  },
+  convertFn: function convert(data) {
     return {
       type: "FeatureCollection",
       features: [
         { type: "Feature", geometry: data.geometry, properties: data.properties }
       ]
     };
-  }`;
+  }
+});
 
-const jsCode = `const map = new maptalks.Map('map', {
-    center: [-74.00912099912109, 40.71107610933129],
-    zoom: 16,
-  });
-  
-  const geo = new maptalks.GeoJSONVectorTileLayer('geo', {
-    data: {
-      "properties": {
-      "adcode": "110000",
-      "name": "北京市",
-      "center": [116.405285, 39.904989],
-      "centroid": [116.41995, 40.18994],
-      "childrenNum": 16,
-      "level": "province",
-      "parent": { "adcode": 100000 },
-      "subFeatureIndex": 0,
-      "acroutes": [100000]
+geo.on("dataload", e => {
+  map.fitExtent(e.extent)
+});
+
+geo.setStyle({
+  style: [
+    {
+      filter: true,
+      renderPlugin: {
+        dataConfig: {
+          type: "point",
+        },
+        sceneConfig: {
+          collision: true,
+          fading: true,
+          depthFunc: "always",
+        },
+        type: "icon",
       },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [116.405285, 39.904989]
+      symbol: {
+        markerBloom: false,
+        markerAllowOverlap: false,
+        markerDx: 0,
+        markerDy: 0,
+        markerFile: null,
+        markerFillOpacity: 1,
+        markerHeight: 21,
+        markerWidth: 21,
+        markerHorizontalAlignment: "middle",
+        markerIgnorePlacement: false,
+        markerLineColor: [0.2, 0.29, 0.39, 1],
+        markerLineDasharray: [0, 0, 0, 0],
+        markerLineOpacity: 1,
+        markerLineWidth: 0,
+        markerOpacity: 1,
+        markerPitchAlignment: "viewport",
+        markerPlacement: "point",
+        markerRotationAlignment: "viewport",
+        markerSpacing: 0,
+        markerType: "ellipse",
+        markerVerticalAlignment: "middle",
       }
-    },
-    convertFn: function convert(data) {
-      return {
-        type: "FeatureCollection",
-        features: [
-          { type: "Feature", geometry: data.geometry, properties: data.properties }
-        ]
-      };
     }
-  });
-
-  geo.on('dataload', e => {
-    map.fitExtent(e.extent)
-  });
+  ]
+})
   
-  const groupLayer = new maptalks.GroupGLLayer('group', [geo]);
-  groupLayer.addTo(map);`;
+const groupLayer = new maptalks.GroupGLLayer("group", [geo]);
+groupLayer.addTo(map);`;
 
 export const convertedGeoDataCodes = {
   html: htmlCode,
--- src/config/codes/vtlayer/layer-operation/load-local-geo.ts
+++ src/config/codes/vtlayer/layer-operation/load-local-geo.ts
@@ -12,62 +12,104 @@ body {
   height: 100%;
 }`;
 
-const jsCode = `const map = new maptalks.Map('map', {
-    center: [-74.00912099912109, 40.71107610933129],
-    zoom: 16,
-  });
+const jsCode = `const map = new maptalks.Map("map", {
+  center: [-74.00912099912109, 40.71107610933129],
+  zoom: 16,
+});
   
-  const geo = new maptalks.GeoJSONVectorTileLayer('geo', {
-    data: {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [120.1862434, 30.31784858]
-          },
-          "properties": {}
+const geo = new maptalks.GeoJSONVectorTileLayer("geo", {
+  data: {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [120.1862434, 30.31784858]
         },
-        {
-          "type": "Feature",
-          "geometry": { "type": "Point", "coordinates": [120.194314, 30.34644838] },
-          "properties": {}
+        properties: {}
+      },
+      {
+        type: "Feature",
+        geometry: { type: "Point", coordinates: [120.194314, 30.34644838] },
+        properties: {}
+      },
+      {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [120.1911453, 30.33728535]
         },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [120.1911453, 30.33728535]
-          },
-          "properties": {}
+        properties: {}
+      },
+      {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [120.1924787, 30.32657846]
         },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [120.1924787, 30.32657846]
-          },
-          "properties": {}
+        properties: {}
+      },
+      {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [120.1723801, 30.27646996]
         },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [120.1723801, 30.27646996]
-          },
-          "properties": {}
-        }
-      ]
-    }
-  });
+        properties: {}
+      }
+    ]
+  }
+});
 
-  geo.on('dataload', e => {
-    map.fitExtent(e.extent)
-  });
+geo.on('dataload', e => {
+  map.fitExtent(e.extent)
+});
+
+geo.setStyle({
+  style: [
+    {
+      filter: true,
+      renderPlugin: {
+        dataConfig: {
+          type: 'point',
+        },
+        sceneConfig: {
+          collision: true,
+          fading: true,
+          depthFunc: 'always',
+        },
+        type: 'icon',
+      },
+      symbol: {
+        markerBloom: false,
+        markerAllowOverlap: false,
+        markerDx: 0,
+        markerDy: 0,
+        markerFile: null,
+        markerFillOpacity: 1,
+        markerHeight: 21,
+        markerWidth: 21,
+        markerHorizontalAlignment: 'middle',
+        markerIgnorePlacement: false,
+        markerLineColor: [0.2, 0.29, 0.39, 1],
+        markerLineDasharray: [0, 0, 0, 0],
+        markerLineOpacity: 1,
+        markerLineWidth: 0,
+        markerOpacity: 1,
+        markerPitchAlignment: 'viewport',
+        markerPlacement: 'point',
+        markerRotationAlignment: 'viewport',
+        markerSpacing: 0,
+        markerType: 'ellipse',
+        markerVerticalAlignment: 'middle',
+      }
+    }
+  ]
+})
   
-  const groupLayer = new maptalks.GroupGLLayer('group', [geo]);
-  groupLayer.addTo(map);`;
+const groupLayer = new maptalks.GroupGLLayer("group", [geo]);
+groupLayer.addTo(map);`;
 
 export const loadLocalGeoDataCodes = {
   html: htmlCode,
