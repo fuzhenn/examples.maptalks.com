299018de11f3737fb07b3ac4168212955bd0a101
deyihu <mehudeyi@163.com>
2022-06-08 14:52:13 +0800

    将所有的例子代码抽离成独立的html作为资源而不是代码

--- src/components/CodeBox/CodePreview/index.tsx
+++ src/components/CodeBox/CodePreview/index.tsx
@@ -5,32 +5,7 @@ import { useStore } from "@/store";
 function CodePreview() {
   const store = useStore();
 
-  const doc = `<!DOCTYPE html>
-  <html>
-  <head>
-  <style type="text/css">
-  ${store.cssCode}
-  </style>
-  <link rel="stylesheet" href="https://unpkg.com/maptalks/dist/maptalks.css">
-  <script type="text/javascript" src="https://unpkg.com/maptalks/dist/maptalks.min.js"></script>
-  <script type="text/javascript" src="https://unpkg.com/@maptalks/gl/dist/maptalksgl.js"></script>
-  <script type="text/javascript" src="https://unpkg.com/@maptalks/vt/dist/maptalks.vt.js"></script>
-  <script type="text/javascript" src="https://unpkg.com/@maptalks/gltf-layer/dist/maptalks.gltf.js"></script>
-  <script type="text/javascript" src="https://unpkg.com/dat.gui/build/dat.gui.min.js"></script>
-  </head>
-  <body>
-  ${store.htmlCode}
-  <script>
-  window.onerror=(message, source, lineno, colno, error) => {
-    if (message.includes("ResizeObserver")) {
-      return;
-    }
-    document.body.innerHTML = "<div>" + message +  "</div>"
-  }
-  ${store.jsCode}
-  </script>
-  </body>
-  </html>`;
+  const doc = store.htmlCode;
 
   return <IFrame sandbox="allow-scripts allow-same-origin" srcDoc={doc} />;
 }
--- src/components/SideBar/DirectoryList/index.tsx
+++ src/components/SideBar/DirectoryList/index.tsx
@@ -2,27 +2,41 @@ import { DataNode, EventDataNode, Key } from "rc-tree/lib/interface";
 
 import { DownOutlined } from "@ant-design/icons";
 import { StyledTree } from "./style";
-import { list } from "@/config";
 import { observer } from "mobx-react-lite";
 import { useStore } from "@/store";
+import { useMount, useUpdateEffect } from "react-use";
+import { useState } from "react";
 
 function DirectoryList() {
+  useMount(() => {
+    fetch(`./resources/examples.json?__time=${new Date().getTime()}`).then(res => res.json()).then(json => {
+      setList(json);
+      setExpandAll(true);
+    })
+  });
   const store = useStore();
 
+  let [list, setList] = useState([]);
+  let [expandAll, setExpandAll] = useState(false);
+
   function handleSelect(keys: Key[], { node }: any) {
-    store.setCurrentKey(keys[0] as string);
+    // store.setCurrentKey(node as string);
+    store.setIsFile(node.isFile as boolean);
+    store.setCurrentKey(node.path as string);
     if (node.href) {
       window.open(node.href);
     }
   }
 
+
   return (
     <StyledTree
-      defaultExpandAll={true}
+      defaultExpandAll={expandAll}
+      autoExpandParent={expandAll}
       showIcon={false}
       defaultSelectedKeys={['0-0-0']}
       switcherIcon={<DownOutlined />}
-      treeData={list as DataNode[]}
+      treeData={list as any[]}
       onSelect={handleSelect}
     />
   );
